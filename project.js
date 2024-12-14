ff6f95199f714384d298365ebe5f5f4cddda8748
Aniket pal <aniket040205@gmail.com>
2024-12-14 20:17:11 +0530

    first commit

---
+++ project.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $(".registration").hide();
+})
+
+
+
+$(document).ready(function(){
+    $(".button1").click(function(){
+        $(".registration").show();
+    })
+})
+
+$(document).ready(function(){
+    $("#btn1").click(function(){
+        $(".registration").hide();
+    })
+})
